84cf5c6cf474529278d29f2ba59c92fdf87c19b2
Karan Verma <karanverma@197NODNB5001651.local>
2018-07-23 16:05:03 +0530

    Set wallpaper from Single View page

--- app/src/main/java/com/clicks/kv/unseenpics/SingleViewActivity.java
+++ app/src/main/java/com/clicks/kv/unseenpics/SingleViewActivity.java
@@ -1,11 +1,27 @@
 package com.clicks.kv.unseenpics;
 
+import android.app.WallpaperManager;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
+import java.io.IOException;
+
 public class SingleViewActivity extends AppCompatActivity {
+
+    Button button;
+    ImageView imageView;
+    WallpaperManager wallpaperManager ;
+    Bitmap bitmap1, bitmap2 ;
+    DisplayMetrics displayMetrics ;
+    int width, height;
+    BitmapDrawable bitmapDrawable ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +37,58 @@ public class SingleViewActivity extends AppCompatActivity {
 
         ImageView imageView = (ImageView) findViewById(R.id.SingleView);
         imageView.setImageResource(imageAdapter.mThumbIds[position]);
+
+
+        button = (Button)findViewById(R.id.set_wallpaper);
+
+        imageView = (ImageView)findViewById(R.id.SingleView);
+
+        wallpaperManager  = WallpaperManager.getInstance(getApplicationContext());
+
+        bitmapDrawable = (BitmapDrawable) imageView.getDrawable();
+
+        bitmap1 = bitmapDrawable.getBitmap();
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                GetScreenWidthHeight();
+
+                SetBitmapSize();
+
+                wallpaperManager = WallpaperManager.getInstance(SingleViewActivity.this);
+
+                try {
+
+                    wallpaperManager.setBitmap(bitmap2);
+
+                    wallpaperManager.suggestDesiredDimensions(width, height);
+
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+    }
+
+    public void GetScreenWidthHeight(){
+
+        displayMetrics = new DisplayMetrics();
+
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+
+        width = displayMetrics.widthPixels;
+
+        height = displayMetrics.heightPixels;
+
+    }
+
+    public void SetBitmapSize(){
+
+        bitmap2 = Bitmap.createScaledBitmap(bitmap1, width, height, false);
+
     }
 }
