9c5999ee382b23ccd9df758e438b5c4662db58bb
Karan Verma <karanverma@kv.local>
2018-08-28 16:18:30 +0530

    change s3

--- app/src/main/java/com/clicks/kv/unseenpics/Constants.java
+++ app/src/main/java/com/clicks/kv/unseenpics/Constants.java
@@ -2,9 +2,9 @@ package com.clicks.kv.unseenpics;
 
 public interface Constants {
 
-    String AWS_ACCESS_KEY = "AKIAJBOOSFXXNZBEGCMA"; // AWS access key
-    String AWS_SECRET_KEY = "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"; // AWS secret key
-    String BUCKET_NAME = "unseenpics"; // AWS bucket name
+    String AWS_ACCESS_KEY = "AKIAICEZSE6YFJ22DOSA"; // AWS access key
+    String AWS_SECRET_KEY = "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"; // AWS secret key
+    String BUCKET_NAME = "unseenwallpaper"; // AWS bucket name
     String KEY = "pics/pic"; // AWS download from key
     String PHOTO_TEMP_PATH = "/data/0/"; // store the downloaded image path
 
